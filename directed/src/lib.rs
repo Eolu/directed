c099999ac6adb9b9a3259fbc5862adcb8bcc2464
Griffin O'Neill <gsoneill1003@gmail.com>
2025-04-26 18:15:50 -0400

    Big README updates

--- directed/src/lib.rs
+++ directed/src/lib.rs
@@ -1,4 +1,4 @@
-//! TODO: Top-level docs
+#![doc = include_str!("../README.md")]
 mod graphs;
 mod node;
 mod registry;
@@ -6,11 +6,6 @@ mod stage;
 mod types;
 mod error;
 
-// TODO: Make a cool visual "rust playgraph" based on this crate
-//    - Ability to create stages, and compile
-//    - Ability to create nodes from stages, and attach them and execute (without recompiling!)
-
-// TODO: Make sure public interface structure makes sense
 pub use directed_stage_macro::stage;
 pub use graphs::{EdgeInfo, Graph};
 pub use node::{AnyNode, Node};
@@ -18,20 +13,6 @@ pub use registry::Registry;
 pub use stage::{EvalStrategy, ReevaluationRule, RefType, Stage};
 pub use types::{DataLabel, NodeOutput};
 pub use error::*;
-
-// TODO: Add a traceable error system, right now it's ignored with anyhow
-// TODO: Graph + Registry could be combined to create a Node (with a baked stage)
-// TODO: An attribute that makes it serialize the cache and store between runs!
-// TODO: Accept inputs for top-level nodes, return outputs from leaf nodes
-// TODO: Automatic validators to make sure correct input and output types are present if required
-// TODO: Caching ALL possible input combinations, not just previous: transparent(cache_all)
-// TODO: A way to reset all registry state at once
-// TODO: A canon way to create a stage out of an entire graph
-//       Right now we combine nodes with stages to make the registry, and registries with graphs.
-//       If we could istead combine STAGES with graphs, then output a valid registry full of nodes
-//       based on that combination, it would avoid the possibility of combining a registry with an
-//       invalid graph entirely. DO THIS!
-// TODO: async support
 
 #[cfg(test)]
 mod tests {
@@ -52,13 +33,13 @@ mod tests {
         #[stage(lazy, cache_last)]
         fn TinyStage2(input: String, input2: String) -> String {
             println!("Running stage 2");
-            input.to_uppercase() + &input2.to_lowercase()
+            input.to_uppercase() + " [" + &input.chars().count().to_string() + " chars]"
         }
 
         #[stage(cache_last)]
         fn TinyStage3(input: String) {
             println!("Running stage 3");
-            assert_eq!("THIS IS THE OUTPUT!this is the output!", input);
+            assert_eq!("THIS IS THE OUTPUT! [19 chars]", input);
         }
 
         let mut registry = Registry::new();
